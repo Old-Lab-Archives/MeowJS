bb2828435ac952c1ecea0b2344b84a0551b50312
Aswini S <ashumeow@live.com>
2014-11-18 16:12:32 +0530

    Create meow.js

---
+++ meow.js
@@ -0,0 +1,39 @@
+function MeowJS()
+{
+  function Meow_Base()
+  {
+    var Meow_Predict = function()
+    {
+      Meow_PredictNumRepDist = 4;
+      Meow_PredictNumStates = 12;
+    }
+    var Meow_InitPredictStates = function()
+    {
+      int Meow_Index;
+      return 0;
+    }
+    var Meow_UpdatePredictStates = function()
+    {
+      int Meow_Index;
+      if(Meow_Index <= 4)
+        {
+          return 0;
+        }
+        if(Meow_Index > 4 && Meow_Index <= 10)
+          {
+            Meow = Meow_Index - 3;
+            Meow = Meow_Index - 6;
+            return Meow;
+          }
+    }
+    var Meow_UpdatePredictStateMatches = function()
+    {
+      int Meow_Index;
+      Meow = Meow_Index < 7 ? 7 : 10;
+      return Meow;
+    }
+    
+    // Still coding... will be updated soon!
+    
+  }
+}
