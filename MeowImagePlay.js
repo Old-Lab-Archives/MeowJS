3f548cd36a9c40a691f242756c22638f4a3cf6d2
Aswini S <ashumeow@live.com>
2014-11-27 06:49:51 +0530

    Update MeowImagePlay.js

--- MeowImagePlay.js
+++ MeowImagePlay.js
@@ -8,6 +8,16 @@ function MeowImagePlay()
 	Meow_ColorPaletteBits = 8;
 	Meow_Color3bits = [];
 	Meow_Color2bits = [];
+	function Meow_ImageByte(Meow_Def4)
+	{
+		var Meow_Def6 = [(Meow_Def4 & 0X7F) >>> 0];
+		while(Meow_Def4 > 127)
+		{
+			Meow_Def4 >>>= 7;
+			Meow_Def6.Meow_Unshift((Meow_Def4 & 0X7f) | 0X80);
+		}
+		return Meow_Def6;
+	}
 
 	// Still coding... Will be updated soon!
 }
