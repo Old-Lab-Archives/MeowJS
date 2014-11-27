7fb5cda83dba5622353aefa579dcdada538f0924
Aswini S <ashumeow@live.com>
2014-11-27 05:52:24 +0530

    Create MeowImagePlay.js

---
+++ MeowImagePlay.js
@@ -0,0 +1,13 @@
+function MeowImagePlay()
+{
+	Meow_ColorFormat_Grey = 'G';
+	Meow_ColorFormat_Alpha = 'A';
+	Meow_ColorFormat_RGB = 'RGB';
+	Meow_ColorFormat_RGBA = 'RGBA';
+	Meow_ColorPalette = 'P';
+	Meow_ColorPaletteBits = 8;
+	Meow_Color3bits = [];
+	Meow_Color2bits = [];
+
+	// Still coding... Will be updated soon!
+}
