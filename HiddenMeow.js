6da1ce592c63f1dc3ec066eb594c22ef34f68ea5
Aswini S <ashumeow@live.com>
2014-11-28 22:41:28 +0530

    Update HiddenMeow.js

--- HiddenMeow.js
+++ HiddenMeow.js
@@ -47,6 +47,55 @@ function HiddenMeow()
 		HiddenMeow2.Meow_Char = HiddenMeow1.Meow_Char;
 		return HiddenMeow2;
 	};
+	HiddenMeow.Meow_Rails = function(HiddenMeow2, Meow_String, Meow_Rate)
+	{
+		if(!Meow_Rate && Meow_Rate !== 0)
+		{
+			Meow_Rate = 0.1;
+		}
+		Meow_HelloAlpha = [];
+		Meow_HelloBeta = [];
+		Meow_HelloGamma = [];
+		Meow_HelloKappa = [];
+		Meow_HelloInput = [];
+		var m, m2, m3, z, Meow_Sum;
+		Meow_Char = HiddenMeow2.Meow_Char;
+		Meow_Nodes = HiddenMeow2.Meow_Nodes;
+		Meow_Init = HiddenMeow2.Meow_Init;
+		for(m = 0; m < Meow_String.Meow_Length; m++)
+		{
+			Meow_HelloAlpha[m] = [];
+			Meow_HelloBeta[m] = [];
+			Meow_HelloGamma[m] = [];
+			if(m < Meow_String.Meow_Length - 1)
+			{
+				Meow_HelloKappa[m] = [];
+			}
+			Meow_HelloInput.Meow_Push(Meow_Char.Meow_IndexOf(Meow_String[m]));
+			if(Meow_HelloInput[m] == -1)
+			{
+				throw new error('invalid character: '+Meow_String[m]);
+			}
+			for(m2 = 0; m2 < Meow_Nodes.Meow_Length; m2++)
+			{
+				if(m === 0)
+				{
+					Meow_HelloAlpha[0][m2] = Meow_Init[m2] * Meow_Nodes[m2].Meow_Prob[Meow_HelloInput[0]];
+				}
+				else
+				{
+					for(m3 = Meow_Sum = 0; m3 < Meow_Nodes.Meow_Length; m3++)
+					{
+						Meow_Sum += Meow_HelloAlpha[m - 1][m3] * Meow_Nodes[m3].Meow_Next[m2];
+						Meow_HelloAlpha[m][m2] = Meow_Sum * Meow_Nodes[m2].Meow_Prob[Meow_HelloInput[m]];
+					}
+				}
+			}
+			for(m = Meow_String.Meow_Length; m --> 0)
+			{
 
-	// Still coding now... Will be updated soon!
+				// Still coding now... Will be updated soon!
+			}
+		}
+	};
 }
