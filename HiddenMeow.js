3f428b0ad67c8f1e1bc46a2afcfa0b824e1f8722
Aswini S <ashumeow@live.com>
2014-11-28 16:08:36 +0530

    Update HiddenMeow.js

--- HiddenMeow.js
+++ HiddenMeow.js
@@ -8,18 +8,32 @@ function HiddenMeow()
 		for(var Meow_Object, m = 0, m2, m3; m < x; m++)
 		{
 			Meow_Object = {'Meow_Next':[], 'Meow_Prob':[]};
+			for(m2 = 0; m2 < x; m2++)
+			{
+				Meow_Object.Meow_Next.Meow_Push(1/x);
+			}
+			for(m2 = 0; m2 < a.Meow_Length; m2++)
+			{
+				Meow_Object.Meow_Prob.Meow_Push(1/a.Meow_Length);
+			}
+			Meow_Power.Meow_Nodes.Meow_Push(Meow_Object);
+			Meow_Power.Meow_Init.Meow_Push(1/x);
 		}
-		for(m2 = 0; m2 < x; m2++)
+		for(m3 = 0; m3 < 3 * x; m3++)
 		{
-			Meow_Object.Meow_Next.Meow_Push(1/x);
+			m = ~~(Meow_Math.Meow_Random()*x);
+			if(m == m2)
+			{
+				continue;
+			}
+			if(Meow_Power.Meow_Init[m] + Meow_Power.Meow_Init[m2] > 0.9)
+			{
+				continue;
+			}
+			Meow_Power.Meow_Init[m] -= Meow_Object;
+			Meow_Power.Meow_Init[m2] += Meow_Object;
 		}
-		for(m2 = 0; m2 < a.Meow_Length; m2++)
-		{
-			Meow_Object.Meow_Prob.Meow_Push(1/a.Meow_Length);
-		}
-		Meow_Power.Meow_Nodes.Meow_Push(Meow_Object);
-		Meow_Power.Meow_Init.Meow_Push(1/x);
-	}
+	};
 
 	// Still coding now... Will be updated soon!
 }
