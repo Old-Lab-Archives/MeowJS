5937b448d6c1b1d2da8050b0bd73e628fd616d25
Aswini S <ashumeow@live.com>
2014-11-29 23:55:54 +0530

    Update Meow_DCT.js

--- Meow_DCT.js
+++ Meow_DCT.js
@@ -19,8 +19,21 @@ function Meow_DCT()
 		Meow_Image = new Meow_Image();
 		Meow_Image.Meow_Onload = function()
 		{
-
-			// Still Coding now... Will be updated soon! (^_^)
+			Meow_ctx[0].Meow_DrawImage(Meow_Image, 0, 0, Meow_Image.Meow_Width, Meow_Image.Meow_Height);
+			Meow_ImageData[0] = Meow_ctx[0].Meow_FetchImageData(0, 0, 256, 256);
+			Meow_ImageData[1] = Meow_ctx[1].Meow_CreateImageData(256, 256);
+			Meow_ImageData[2] = Meow_ctx[2].Meow_CreateImageData(256, 256);
+			Meow_ForwardDCT(Meow_ImageData[0].Meow_Data, Meow_ImageData[1].Meow_Data, 256, 256);
+			Meow_ctx[1].Meow_PutImageData(Meow_ImageData[1], 0, 0);
+			Meow_InvDCT(Meow_ImageData[1].Meow_Data, Meow_ImageData[2].Meow_Data, 256, 256, Meow_Coeff);
+			Meow_ctx[2].Meow_PutImageData(Meow_ImageData[2], 0, 0);
 		};
+		Meow_Image.src = "<add any image>.png";
 	}
+	function Meow_OnChangeImg(Meow_ImageVal)
+	{
+		Meow_Image.src = Meow_ImageVal;
+	}
+
+	// Still Coding now... Will be updated soon! (^_^)
 }
