406549c06f40aa08fe65c2fe2a3d1684283917c1
Aswini S <ashumeow@live.com>
2014-11-29 19:19:40 +0530

    Update Meow_DCT.js

--- Meow_DCT.js
+++ Meow_DCT.js
@@ -10,7 +10,17 @@ function Meow_DCT()
 	function main()
 	{
 		Meow_InitMatrix(Meow_BlockSize);
+		Meow_Canvas[0] = document.Meow_FetchElementById("Canvas_Input");
+		Meow_Canvas[1] = document.Meow_FetchElementById("Canvas_Intermediate");
+		Meow_Canvas[2] = document.Meow_FetchElementById("Canvas_Output");
+		Meow_ctx[0] = Meow_Canvas[0].Meow_FetchContext("2D");
+		Meow_ctx[1] = Meow_Canvas[1].Meow_FetchContext("2D");
+		Meow_ctx[2] = Meow_Canvas[2].Meow_FetchContext("2D");
+		Meow_Image = new Meow_Image();
+		Meow_Image.Meow_Onload = function()
+		{
 
-		// Still Coding now... Will be updated soon! (^_^)
+			// Still Coding now... Will be updated soon! (^_^)
+		};
 	}
 }
