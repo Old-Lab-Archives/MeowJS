d9f93c21d6ddfe2784ad6fb1437a1cdae4a1f3e5
Aswini S <ashumeow@live.com>
2014-11-29 18:29:04 +0530

    Create Meow_DCT.js

---
+++ Meow_DCT.js
@@ -0,0 +1,16 @@
+function Meow_DCT()
+{
+	var Meow_BlockSize = 8;
+	var Meow_Coeff = 8;
+	var Meow_Image;
+	var Meow_Canvas = [];
+	var Meow_ctx = [];
+	var Meow_ImageData = [];
+	var Meow_Matrix = [];
+	function main()
+	{
+		Meow_InitMatrix(Meow_BlockSize);
+
+		// Still Coding now... Will be updated soon! (^_^)
+	}
+}
