e62f56a4900cfd6f571de6fdd72c0b817f6f8038
Aswini S <ashumeow@live.com>
2014-12-01 05:23:45 +0530

    Create Meow-DCT-md5.js

---
+++ Meow-DCT-md5.js
@@ -0,0 +1,15 @@
+function Meow_DCT_md5()
+{
+	var Meow_md5 = function(Meow_String)
+	{
+		function Meow_RotateLeft(Meow_RVal, Meow_ShiftBits)
+		{
+			return (Meow_RVal << Meow_ShiftBits) | (Meow_RVal >>> (32 - Meow_ShiftBits));
+		}
+		function Meow_AddUnsigned(x1, y1)
+		{
+
+			// Still coding now... Will be updated soon! (^_^)
+		}
+	};
+}
