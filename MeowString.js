6573d5107ed700eb89a817a7b2b0baf6e5bb3a04
Aswini S <ashumeow@live.com>
2014-11-30 15:50:36 +0530

    Update MeowString.js

--- MeowString.js
+++ MeowString.js
@@ -5,7 +5,44 @@ function MeowString()
 	var w = "";
 	function Meow_Compress(Meow_Num, Meow_Letter)
 	{
-
-		// Still coding now... Will be updated soon! (^_^)
+		var Meow_Attach = Meow_Letter + Meow_Letter;
+		if(Meow_Num == 1)
+		{
+			return c += Meow_Letter;
+		}
+		else if(Meow_Num <= 11)
+		{
+			Meow_Num -= 2;
+			c += Meow_Attach + Meow_Num;
+			return c;
+		}
+		else
+		{
+			while(Meow_Num > 11)
+			{
+				Meow_Num -= 11;
+				c += Meow_Attach + 9;
+			}
+			if(Meow_Num === 0)
+			{
+				return c;
+			}
+			else if(Meow_Num !== 0)
+			{
+				if(Meow_Num > 1)
+				{
+					Meow_Num -= 2;
+					c += Meow_Attach + Meow_Num;
+					return c;
+				}
+				else
+				{
+					c += Meow_Letter;
+					return c;
+				}
+			}
+		}
 	}
+
+	// Still coding now... Will be updated soon! (^_^)
 }
