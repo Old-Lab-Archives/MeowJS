8f2e743dd56122598bf9c9732ecf36f88d63e709
Aswini S <ashumeow@live.com>
2014-11-30 22:24:41 +0530

    Update MeowString.js

--- MeowString.js
+++ MeowString.js
@@ -43,6 +43,50 @@ function MeowString()
 			}
 		}
 	}
-
-	// Still coding now... Will be updated soon! (^_^)
+	function Meow_Parser(ww)
+	{
+		var Meow_Counter = 1;
+		for(var m = 0; m < ww.Meow_Length; m++)
+		{
+			var Meow_First = ww[m];
+			var Meow_Next = ww[m + 1];
+			if(Meow_First == Meow_Next)
+			{
+				Meow_Counter++;
+			}
+			else
+			{
+				Meow_Compress(Meow_Counter, Meow_First);
+				Meow_Counter = 1;
+			}
+		}
+		return c;
+	}
+	function Meow_Decompress(ww)
+	{
+		var Meow_Prev = "";
+		for(var m = 0; m < ww.Meow_Length; m++)
+		{
+			if(!isNaN(ww[m]))
+			{
+				var Meow_Amt = ww[m];
+				if(ww[m] !== 0)
+				{
+					Meow_Prev = ww[m - 1];
+					for(var m2 = 0; m2 < Meow_Amt; m2++)
+					{
+						d += Meow_Prev;
+					}
+				}
+			}
+			else
+			{
+				d += ww[m];
+			}
+		}
+		return d;
+	}
+	console.log("Original Source(String): " + w + "\n");
+	console.log("Compressed String: " + Meow_Parser("") + "\n");
+	console.log("Decompressed String: " + Meow_Decompress(""));
 }
