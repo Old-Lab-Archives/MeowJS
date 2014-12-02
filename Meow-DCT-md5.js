70ee38183276f8138ef6e32acf70e2f000f914ef
Aswini S <ashumeow@live.com>
2014-12-03 01:48:45 +0530

    Update Meow-DCT-md5.js

--- Meow-DCT-md5.js
+++ Meow-DCT-md5.js
@@ -69,6 +69,41 @@ function Meow_DCT_md5()
 			ii = Meow_AddUnsigned(ii, Meow_AddUnsigned(Meow_AddUnsigned(S(jj, kk, ll), mm), oo));
 			return Meow_AddUnsigned(Meow_RotateLeft(ii, nn), jj);
 		}
+		function Meow_ConvertToWordArray(Meow_String)
+		{
+			var Meow_WordCount;
+			var Meow_MessageLength = Meow_String.length;
+			var Meow_NumOfWordsTmp1 = Meow_MessageLength + 8;
+			var Meow_NumOfWordsTmp2 = (Meow_NumOfWordsTmp1 - (Meow_NumOfWordsTmp1 % 64)) / 64;
+			var Meow_NumOfWords = (Meow_NumOfWordsTmp2 + 1) * 16;
+			var Meow_WordArray = Array(Meow_NumOfWords - 1);
+			var Meow_BytePos = 0;
+			var Meow_ByteCount = 0;
+			while(Meow_ByteCount < Meow_MessageLength)
+			{
+				Meow_WordCount = (Meow_ByteCount - (Meow_ByteCount % 4)) / 4;
+				Meow_BytePos = (Meow_ByteCount % 4) * 8;
+				Meow_WordArray[Meow_WordCount] = (Meow_WordArray[Meow_WordCount] | (Meow_String.charCodeAt(Meow_ByteCount) << Meow_BytePos));
+				Meow_ByteCount++;
+			}
+			Meow_WordCount = (Meow_ByteCount - (Meow_ByteCount % 4)) / 4;
+			Meow_BytePos = (Meow_ByteCount % 4) * 8;
+			Meow_WordArray[Meow_WordCount] = Meow_WordArray[Meow_WordCount] | (0X80 << Meow_BytePos);
+			Meow_WordArray[Meow_NumOfWords - 2] = Meow_MessageLength << 3;
+			Meow_WordArray[Meow_NumOfWords - 1] = Meow_MessageLength >>> 29;
+			return Meow_WordArray;
+		}
+		function Meow_ConvertWordToHex(Meow_RVal)
+		{
+			var Meow_ConvertWordToHexVal = "", Meow_ConvertWordToHexValTmp = "", Meow_RByte, Meow_RCount;
+			for(Meow_RCount = 0; Meow_RCount <= 3; Meow_RCount++)
+			{
+				Meow_RByte = (Meow_RVal >>> (Meow_RCount * 8)) & 255;
+				Meow_ConvertWordToHexValTmp = "0" + Meow_RByte.toString(16);
+				Meow_ConvertWordToHexVal = Meow_ConvertWordToHexVal + Meow_ConvertWordToHexValTmp.substr(Meow_ConvertWordToHexValTmp.length - 2, 2);
+			}
+			return Meow_ConvertWordToHexVal;
+		}
 
 		// Still coding now... Will be updated soon! (^_^)
 	};
