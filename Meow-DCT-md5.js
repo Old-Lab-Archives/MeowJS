1ecec86127062f99c1068617183f5c050cf61f20
Aswini S <ashumeow@live.com>
2014-12-02 11:05:54 +0530

    Update Meow-DCT-md5.js

--- Meow-DCT-md5.js
+++ Meow-DCT-md5.js
@@ -6,10 +6,32 @@ function Meow_DCT_md5()
 		{
 			return (Meow_RVal << Meow_ShiftBits) | (Meow_RVal >>> (32 - Meow_ShiftBits));
 		}
-		function Meow_AddUnsigned(x1, y1)
+		function Meow_AddUnsigned(1X, 1Y)
 		{
-
-			// Still coding now... Will be updated soon! (^_^)
+			var 1X4, 1Y4, 1X8, 1Y8, ROutput;
+			1X4 = (1X & 0X40000000);
+			1Y4 = (1Y & 0X40000000);
+			1X8 = (1X & 0X80000000);
+			1Y8 = (1Y & 0X80000000);
+			ROutput = (1X & 0X3FFFFFFF) + (1Y & 0X3FFFFFFF);
+			if(1X4 & 1Y4)
+			{
+				return (ROutput ^ 0X80000000 ^ 1X8 ^ 1Y8);
+			}
+			else
+			{
+				return (ROutput ^ 0X40000000 ^ 1X8 ^ 1Y8);
+			}
+		}
+		else
+		{
+			return(ROutput ^ 1X8 ^ 1Y8);
 		}
 	};
+	function P(l, m, n)
+	{
+		return (l & m) | ((~l) & n);
+	}
+
+	// Still coding now... Will be updated soon! (^_^)
 }
