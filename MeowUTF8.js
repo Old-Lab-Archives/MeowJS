e65dd6331eae0de41c01dc6b9de588b275de13cf
Aswini S <ashumeow@live.com>
2014-12-05 05:22:48 +0530

    Create MeowUTF8.js

---
+++ MeowUTF8.js
@@ -0,0 +1,11 @@
+var MeowUTF8 = (function(Meow_Root, undefined) {
+	'use strict';
+	var Meow_HexChar = "0123456789abcdef";
+	var Meow_HexTable = {
+		'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+		'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15,
+		'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
+	};
+
+	// Still coding now... Will be updated soon! (^_^)
+});
