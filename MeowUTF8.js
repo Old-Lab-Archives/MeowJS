ca69622d7c3c0531f7f4d0a0b1133d2d7f86f110
Aswini S <ashumeow@live.com>
2014-12-05 08:54:53 +0530

    Update MeowUTF8.js

--- MeowUTF8.js
+++ MeowUTF8.js
@@ -27,12 +27,58 @@ var EE = [0XD76AA478, 0XE8C7B756, 0X242070DB, 0XC1BDCEEE,
 	0X6FA87E4F, 0XFE2CE6E0, 0XA3014314, 0X4E0811A1,
 	0XF7537E82, 0XBD3AF235, 0X2AD7D2BB, 0XEB86D391];
 var Meow_UTFmd5 = function(Meow_Msg) {
-	var Meow_Block = new Meow_HasUTF8(Meow_Msg) ? new Meow_UTF8toBlocks(Meow_Msg) : new Meow_ASCIItoBlocks(Meow_Msg);
-	var E0 = 0X67452301;
-	var E1 = 0XEFCDAB89;
-	var E2 = 0X98BADCFE;
-	var E3 = 0X10325476;
+var Meow_Block = new Meow_HasUTF8(Meow_Msg) ? new Meow_UTF8toBlocks(Meow_Msg) : new Meow_ASCIItoBlocks(Meow_Msg);
+var E0 = 0X67452301;
+var E1 = 0XEFCDAB89;
+var E2 = 0X98BADCFE;
+var E3 = 0X10325476;
+for(var m = 0, Meow_Len = Meow_Block.length; m < Meow_Len; m += 16)
+{
+	var ii = E0;
+	var jj = E1;
+	var kk = E2;
+	var ll = E3;
+	var p, q, Meow_Tmp, xxx, y;
+	for(var m2 = 0; m2 < 64; ++m2)
+	{
+		if(m2 < 16)
+		{
+			p = ll ^ (jj & (kk ^ ll));
+			q = m2;
+		}
+		else if(m2 < 32)
+		{
+			p = kk ^ (ll & (jj ^ kk));
+			q = (5 * m2 + 1) % 16;
+		}
+		else if(m2 < 48)
+		{
+			p = jj ^ kk ^ ll;
+			q = (3 * m2 + 5) % 16;
+		}
+		else
+		{
+			p = kk ^ (jj | (~ll));
+			q = (7 * m2) % 16;
+		}
+		Meow_Tmp = ll;
+		ll = kk;
+		kk = jj;
+		xxx = (ii + p + m3[m2] + Meow_Block[m + q]);
+		y = lR[m2];
+		jj += (xxx << y) | (xxx >>> (32 - y));
+		ii = Meow_Tmp;
+	}
+	E0 = (E0 + ii) | 0;
+	E1 = (E1 + jj) | 0;
+	E2 = (E2 + kk) | 0;
+	E3 = (E3 + ll) | 0;
+	}
+	return new Meow_ToHexStr(E0) + new Meow_ToHexStr(E1) + new Meow_ToHexStr(E2) + new Meow_ToHexStr(E3);
+	};
+	var Meow_ToHexStr = function(Meow_Num)
+	{
 
-	// still coding now... will be updated soon!
-};
+		// still coding now... will be updated soon!
+	};
 });
