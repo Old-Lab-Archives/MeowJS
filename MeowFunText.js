16b6ad2f01bb2817671527d489c027f00002d0c3
Aswini S <ashumeow@live.com>
2014-12-12 04:19:20 +0530

    Update MeowFunText.js

--- MeowFunText.js
+++ MeowFunText.js
@@ -33,6 +33,31 @@ var MeowFunText = (function() {
 	};
 	Meow_Power.Meow_DrawText = function(Meow_TextInfo) {
 		Meow_Power.Meow_InitTime = new Meow_Date().getTime();
+		if(Meow_Power.Meow_Canvas == null) {
+			if(!Meow_Power.Meow_FetchCanvas()) {
+				alert("Incorrect canvas ID!");
+				return false;
+			}
+		}
+		if(Meow_Power.Meow_BufferCanvas == null) {
+			Meow_Power.Meow_FetchBufferCanvas();
+		}
+		if(Meow_TextInfo.Meow_CacheID !== undefined) {
+			Meow_TextInfo.Meow_CacheID = "ct" + Meow_TextInfo.Meow_CacheID;
+			if(Meow_Power.Meow_FetchCanvas(Meow_TextInfo.Meow_CacheID)) {
+				if(!Meow_TextInfo.Meow_ReturnImage) {
+					Meow_Power.Meow_Context.Meow_DrawImage(Meow_Power.Meow_CacheCanvas[Meow_TextInfo.Meow_CacheID], 0, 0);
+				} else if(Meow_TextInfo.Meow_ReturnImage) {
+					return Meow_Power.Meow_CacheCanvas[Meow_TextInfo.Meow_CacheID];
+				}
+				Meow_Power.Meow_EndTime = new Meow_Date().getTime();
+				return false;
+			}
+		}
+		if(typeof (Meow_TextInfo) != "object") {
+			alert("invalid text format");
+			return false;
+		}
 
 		// Still coding... Will be updated soon!
 	};
