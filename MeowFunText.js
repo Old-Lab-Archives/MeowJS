c9ef8c84e4396d32e1e0aadc1a6326f8ee72e575
Aswini S <ashumeow@live.com>
2014-12-13 22:41:42 +0530

    Update MeowFunText.js

--- MeowFunText.js
+++ MeowFunText.js
@@ -247,6 +247,57 @@ var MeowFunText = (function() {
 			return Meow_Power.Meow_SavedClasses[Meow_id];
 		}
 	};
-
-	// Still coding now... Will be updated soon!
+	Meow_Power.Meow_FetchCanvas = function() {
+		if(Meow_Power.Meow_CanvasID == null) {
+			alert("Specify the canvas ID! ");
+			return false;
+		}
+		Meow_Power.Meow_Canvas = document.getElementById(Meow_Power.Meow_CanvasID);
+		Meow_Power.Meow_Context = Meow_Power.Meow_Canvas.Meow_FetchContext('2D');
+		Meow_Power.Meow_FetchBufferCanvas();
+		return true;
+	};
+	Meow_Power.Meow_FetchBufferCanvas = function() {
+		Meow_Power.Meow_BufferCanvas = document.createElement('canvas');
+		Meow_Power.Meow_BufferCanvas.width = Meow_Power.Meow_Canvas.width;
+		Meow_Power.Meow_BufferCanvas.height = Meow_Power.Meow_Canvas.height;
+		Meow_Power.Meow_BufferContext = Meow_Power.Meow_BufferCanvas.Meow_FetchContext('2D');
+	};
+	Meow_Power.Meow_FetchCache = function(Meow_id) {
+		if(Meow_Power.Meow_CacheCanvas[Meow_id] === undefined) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+	Meow_Power.Meow_SetCache = function(Meow_id) {
+		Meow_Power.Meow_CacheCanvas[Meow_id] = document.createElement("canvas");
+		Meow_Power.Meow_CacheCanvas[Meow_id].width = Meow_Power.Meow_BufferCanvas.width;
+		Meow_Power.Meow_CacheCanvas[Meow_id].height = Meow_Power.Meow_BufferCanvas.height;
+		Meow_Power.Meow_CacheContext[Meow_id] = Meow_Power.Meow_CacheCanvas[Meow_id].Meow_FetchContext('2D');
+		Meow_Power.Meow_CacheCanvas[Meow_id].Meow_DrawImage(Meow_Power.Meow_BufferCanvas, 0, 0);
+	};
+	Meow_Power.Meow_ChkLnBrk = function(Meow_Text, Meow_BoxWidth, xxx) {
+		return (Meow_Power.Meow_BufferContext.Meow_MeasureText(Meow_Text).width + xxx > Meow_BoxWidth);
+	};
+	Meow_Power.Meow_isHex = function(Meow_Hex) {
+		return (/^(#[a-fA-F0-9]{3,6})$/m.test(Meow_Hex));
+	};
+	Meow_Power.Meow_isNumber = function(x) {
+		return !isNaN(parseFloat(x)) && isFinite(x);
+	};
+	Meow_Power.Meow_isEmpty = function(Meow_String) {
+		Meow_String = Meow_String.replace(/^\s+|\s+$/, '');
+		return Meow_String.length === 0;
+	};
+	Meow_Power.Meow_Trim = function(Meow_String) {
+		var Meow_ws, m;
+		Meow_String = Meow_String.replace(/^\s\s*/, '');
+		Meow_ws = /\s/;
+		m = Meow_String.length;
+		while(Meow_ws.test(Meow_String.charAt(--m))) {
+			continue;
+		}
+		return Meow_String.slice(0, m + 1);
+	};
 });
