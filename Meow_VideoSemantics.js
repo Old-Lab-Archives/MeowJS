087a962b182c94cb588edf45bec22ff81bf35e07
Aswini S <ashumeow@live.com>
2014-12-22 06:31:05 +0530

    Create Meow_VideoSemantics.js

---
+++ Meow_VideoSemantics.js
@@ -0,0 +1,8 @@
+window.Meow_VideoSemantics = (function(window, document, undefined) {
+	'use strict';
+	var MeowVideoSemantics = function(Meow_VidElement) {
+		Meow_Power.Meow_VidElement = Meow_VidElement;
+	};
+
+	//Still coding... will be updated soon!
+});
