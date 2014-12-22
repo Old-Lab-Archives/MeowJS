e65788e23f9f584d13da0b367e2680e3661f75b0
Aswini S <ashumeow@live.com>
2014-12-22 07:28:31 +0530

    Create MeowUTF8_16.js

---
+++ MeowUTF8_16.js
@@ -0,0 +1,11 @@
+var MeowUTF8_16 = (function(Meow_Global, Meow_String) {
+	"use strict";
+	if(!Array.Meow_isArray) {
+		Array.Meow_isArray = function(vvv) {
+			return Object.prototype.toString.call(vvv) === "[Object Array]";
+		};
+	}
+	var Meow_StringFromCharCode = String.fromCharCode;
+
+	// Still coding... will be updated soon!
+});
