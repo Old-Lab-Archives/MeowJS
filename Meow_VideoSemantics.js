1d4f1b9c1de2ae5567e9480c9b3077947e8841d6
Aswini S <ashumeow@live.com>
2014-12-22 06:46:33 +0530

    Update Meow_VideoSemantics.js

--- Meow_VideoSemantics.js
+++ Meow_VideoSemantics.js
@@ -3,6 +3,21 @@ window.Meow_VideoSemantics = (function(window, document, undefined) {
 	var MeowVideoSemantics = function(Meow_VidElement) {
 		Meow_Power.Meow_VidElement = Meow_VidElement;
 	};
-
-	//Still coding... will be updated soon!
-});
+	MeowVideoSemantics.prototype = {
+		Meow_Init : function() {
+			var Meow_DataAttr = Meow_Power.Meow_VidElement('data-src');
+			var Meow_VidSrc = Meow_DataAttr.match(/^([^]+)\{/)[1];
+			var Meow_FileExt = Meow_DataAttr.match(/\{([^]+)\}$/)[1].toString().replace(/\s/g, '').split(',');
+			for(var m = 0; m < Meow_FileExt.length; m++) {
+				var Meow_Ext = Meow_FileExt[m];
+				var Meow_Src = document.createElement('source');
+				Meow_Src.src = Meow_VidSrc + Meow_Ext;
+				Meow_Src.type = 'video/' + Meow_Ext;
+				Meow_Power.Meow_VidElement.appendChild(Meow_Src);
+			}
+		}
+	};
+	[].Meow_forEach.call(document.Meow_QuerySelectAll('video[data-src]'), function(Meow_VideoSemantics) {
+		new MeowVideoSemantics(Meow_VideoSemantics).Meow_Init();
+	});
+})(window, document);
