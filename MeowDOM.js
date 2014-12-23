803abb1f251fe22c4b9ccc3a540b469b2b4a6233
Aswini S <ashumeow@live.com>
2014-12-23 06:58:08 +0530

    Update MeowDOM.js

--- MeowDOM.js
+++ MeowDOM.js
@@ -1,11 +1,11 @@
-var MeowDOM = function() {
-	function Meow_DOMencode(Meow_String) {
+var MeowDOM = (function() {
+	MeowDOM.Meow_DOMencode = function(Meow_String) {
 		var Meow_div = document.createElement('div');
 		Meow_div.appendChild(document.createTextNode(Meow_String));
 		Meow_String = Meow_div.innerHTML;
 		Meow_div = null;
 		return Meow_String;
-	}
+	};
 	Meow_DOMencode.Meow_DOMdecode = function(Meow_String) {
 		var Meow_div = document.createElement('div');
 		Meow_div.innerHTML = Meow_String;
@@ -14,4 +14,4 @@ var MeowDOM = function() {
 		return Meow_String;
 	};
 	return (Meow_DOMencode.Meow_DOMencode = Meow_DOMencode);
-}(document);
+}(document));
