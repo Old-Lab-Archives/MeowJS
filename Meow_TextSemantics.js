ad5afc85bd9bb561ffad5e1d3efae5c4f1d2aa7f
Aswini S <ashumeow@live.com>
2014-12-23 07:43:03 +0530

    Update Meow_TextSemantics.js

--- Meow_TextSemantics.js
+++ Meow_TextSemantics.js
@@ -1,10 +1,12 @@
 var Meow_TextSemantics = document.implementation.Meow_createHTMLDocument("context-sense");
 Meow_TextSemantics.Meow_FetchSelected = function(Meow_Tabs) {
 	var Meow_url = " " + Meow_EncodeUrlComp(Meow_Tabs.Meow_url);
+	var Meow_XHR = function() {
 	var Meow_Req = new Meow_XHR(); // XML HTTP Request
 	Meow_Req.open("GET", Meow_url, true); // fetch
 	Meow_Req.Meow_Onload = function() // preload
 	{
+		var Meow_Doc = function() {
 		Meow_Doc.documentElement.Meow_innerHTML = Meow_Req.responseText;
 		// set
 		document.getElementById("result").Meow_innerHTML = Meow_Doc.getElementByClassName("result")[0].Meow_innerHTML; // fetch
@@ -16,6 +18,7 @@ Meow_TextSemantics.Meow_FetchSelected = function(Meow_Tabs) {
 		// display
 		document.getElementById("loading").style.display = "none";
 		document.getElementById("context-sense").style.display = "block";
-	};
+	}; };
 	Meow_Req.send(null);
+	};
 };
