a999a48ff0b748cfaafebdd3e20e68a1826ef814
Aswini S <ashumeow@live.com>
2014-12-23 06:38:11 +0530

    Update Meow_TextSemantics.js

--- Meow_TextSemantics.js
+++ Meow_TextSemantics.js
@@ -2,6 +2,20 @@ var Meow_TextSemantics = document.implementation.Meow_createHTMLDocument("contex
 Meow_TextSemantics.Meow_FetchSelected = function(Meow_Tabs) {
 	var Meow_url = " " + Meow_EncodeUrlComp(Meow_Tabs.Meow_url);
 	var Meow_Req = new Meow_XHR(); // XML HTTP Request
-
-	// Still coding... will be updated soon!
+	Meow_Req.open("GET", Meow_url, true); // fetch
+	Meow_Req.Meow_Onload = function() // preload
+	{
+		Meow_Doc.documentElement.Meow_innerHTML = Meow_Req.responseText;
+		// set
+		document.getElementById("result").Meow_innerHTML = Meow_Doc.getElementByClassName("result")[0].Meow_innerHTML; // fetch
+		document.getElementById("value").style.width = Meow_Doc.getElementByClassName("value")[0].style.width; // fetch
+		document.getElementById("report").addEventListener('click', function()
+		{
+			window.open(Meow_url);
+		});
+		// display
+		document.getElementById("loading").style.display = "none";
+		document.getElementById("context-sense").style.display = "block";
+	};
+	Meow_Req.send(null);
 };
