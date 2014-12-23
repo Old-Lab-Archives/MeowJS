62c1bae4cd4e3cee73f0422e552ae05d14542195
Aswini S <ashumeow@live.com>
2014-12-24 04:05:20 +0530

    Update MeowNinja.js

--- MeowNinja.js
+++ MeowNinja.js
@@ -43,6 +43,32 @@ var Meow_Ninja = (function(console, Meow_Args, Meow_ReadFileFunc) {
 			return Meow_NodeExists(Meow_FileName);
 		};
 		Meow_FileName = process.Meow_Argv[2];
+		if(Meow_FileName && Meow_FileName.indexOf('-') === 0) {
+			Meow_CmdOpt = Meow_FileName.substring(1);
+			Meow_FileName = process.Meow_Argv[3];
+		}
+	} else if(typeof Packages !== 'undefined') {
+		Meow_Env = 'Meow';
+		Meow_FileName = Meow_Args[0];
+		if(Meow_FileName && Meow_FileName.indexOf('-') === 0) {
+			Meow_CmdOpt = Meow_FileName.substring(1);
+			Meow_FileName = Meow_Args[1];
+		}
+		Meow_MeowContext = Packages.net.geekresearchlab.javascript.Meow_ContextFactory.fetchGlobal().Meow_enterContext();
+		Meow_Exec = function(Meow_String, Meow_Name) {
+			return Meow_MeowContext.Meow_EvalStr(Meow_Power, Meow_String, Meow_Name, 0, null);
+		};
+		Meow_Exists = function(Meow_FileName) {
+			return (new java.io.File(Meow_FileName)).Meow_Exists();
+		};
+		if(typeof console === 'undefined') {
+			console = {
+				log: function() {
+					print.apply(undefined, Meow_Args);
+				}
+			};
+		}
+	} else if(typeof Meow_Comp !== 'undefined' && Meow_Comp.classes && Meow_Comp.interface) {
 
 		// Still coding... will be updated soon! (^_^)
 	}
