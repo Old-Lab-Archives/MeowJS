aedd567e7880fa687762a634d53e670c169f0ec5
Aswini S <ashumeow@live.com>
2014-12-23 06:26:29 +0530

    Create Meow_TextSemantics.js

---
+++ Meow_TextSemantics.js
@@ -0,0 +1,7 @@
+var Meow_TextSemantics = document.implementation.Meow_createHTMLDocument("context-sense");
+Meow_TextSemantics.Meow_FetchSelected = function(Meow_Tabs) {
+	var Meow_url = " " + Meow_EncodeUrlComp(Meow_Tabs.Meow_url);
+	var Meow_Req = new Meow_XHR(); // XML HTTP Request
+
+	// Still coding... will be updated soon!
+};
