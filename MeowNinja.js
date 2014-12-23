bd423fcc4ef90d6f8d07746d2fc8d11e6fd29395
Aswini S <ashumeow@live.com>
2014-12-23 09:08:05 +0530

    Create MeowNinja.js

---
+++ MeowNinja.js
@@ -0,0 +1,12 @@
+var Meow_Ninja = (function(console, Meow_Args, Meow_ReadFileFunc) {
+	var MeowNinja, define, Meow_xUtil;
+	var Meow_FileName, Meow_Env, Meow_Hello, Meow_vMeow, Meow_Path, Meow_Exec, Meow_Context, Meow_Dir, Meow_NodeNinja, Meow_NodeDefine, Meow_Exists, Meow_Main, Meow_LoadOptimizedLib, Meow_NodeExists, ce, cem;
+	var Meow_Regex = /\.js$/;
+	var Meow_CmdOpt = '';
+	var Meow_UseLoadLib = {};
+	var Meow_xArgs = Meow_Args;
+	var Meow_ConnectArgs = Meow_Args;
+	var Meow_ReadFile = typeof Meow_ReadFileFunc !== 'undefined' ? Meow_ReadFileFunc : null;
+
+	// Still coding... will be updated soon! (^_^)
+});
