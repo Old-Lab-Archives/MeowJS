990e4f3229f19ee4462b7c29e6c04a647804daec
Aswini S <ashumeow@live.com>
2014-12-23 07:07:55 +0530

    Update MeowDOM.js

--- MeowDOM.js
+++ MeowDOM.js
@@ -13,7 +13,7 @@ var MeowDOM = (function() {
 		Meow_div = null;
 		return Meow_String;
 	};
-	Meow_DOMencode.Meow_DOMencode = function() {
-	return (Meow_DOMencode.Meow_DOMencode = Meow_DOMencode);
+	Meow_DOMencode.Meow_DOMencode = function(Meow_DOMencode) {
+	return;
 	};
 }(document));
