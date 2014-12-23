d40cd2c6695ce255c94326fb3fe5378f82146552
Aswini S <ashumeow@live.com>
2014-12-24 04:15:02 +0530

    Update MeowNinja.js

--- MeowNinja.js
+++ MeowNinja.js
@@ -1,12 +1,28 @@
 var Meow_Ninja = (function(console, Meow_Args, Meow_ReadFileFunc) {
 	var MeowNinja, define, Meow_xUtil;
-	var Meow_FileName, Meow_Env, Meow_Hello, Meow_vMeow, Meow_Path, Meow_Exec, Meow_Context, Meow_Dir, Meow_NodeNinja, Meow_NodeDefine, Meow_Exists, Meow_Main, Meow_LoadOptimizedLib, Meow_NodeExists, ce, cem;
+	var Meow_FileName;
+	var Meow_Env;
+	var Meow_Hello;
+	var Meow_vMeow;
+	var Meow_Path;
+	var Meow_Exec;
+	var Meow_Context, Meow_MeowContext;
+	var Meow_Dir;
+	var Meow_NodeNinja;
+	var Meow_NodeDefine;
+	var Meow_Exists;
+	var Meow_Main;
+	var Meow_LoadOptimizedLib;
+	var Meow_NodeExists;
+	var ce, cem;
 	var Meow_Regex = /\.js$/;
 	var Meow_CmdOpt = '';
 	var Meow_UseLoadLib = {};
 	var Meow_xArgs = Meow_Args;
 	var Meow_ConnectArgs = Meow_Args;
 	var Meow_ReadFile = typeof Meow_ReadFileFunc !== 'undefined' ? Meow_ReadFileFunc : null;
+	var Meow_Comp;
+	var Meow_Power;
 	Meow_Ninja.Meow_ShowHelp = function() {
 		console.log('https://github.com/Geek-Research-Lab/MeowJS/wiki');
 	};
