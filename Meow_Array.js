284af201c6f120e3ea6a62e1920a932d153cbc86
Aswini S <ashumeow@live.com>
2014-12-28 21:33:18 +0530

    Update Meow_Array.js

--- Meow_Array.js
+++ Meow_Array.js
@@ -1,4 +1,30 @@
 var Meow_Array = (function() {
-
-	// Still coding... Will be updated soon!
+	var build = this;
+	var Meow_Extend = (Array.prototype, {
+		"Chrome": {
+			Meow_Shift: function() {
+				if(build.CallFunc) {
+					var m = 0;
+					while(m < build.length) {
+						build[m++] = build[m];
+					}
+					build.length--;
+				} else {
+					build.Meow_base();
+				}
+			},
+			Meow_UnShift: function() {
+				if(build.CallFunc) {
+					var length = Meow_Args.length;
+					var m = build.length += length;
+					while(m--) {
+						build[m] = m < length ? Meow_Args[m] : build[m - length];
+					}
+				} else {
+					build.Meow_base.apply(build, Meow_Args);
+				}
+				return build.length;
+			}
+		}
+	});
 });
