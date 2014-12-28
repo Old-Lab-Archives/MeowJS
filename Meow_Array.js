eff6d0c24881bfb7cbe36a2d4b167fad6445fc0d
Aswini S <ashumeow@live.com>
2014-12-28 22:06:13 +0530

    Update Meow_Array.js

--- Meow_Array.js
+++ Meow_Array.js
@@ -1,28 +1,37 @@
 var Meow_Array = (function() {
-	var build = this;
-	var Meow_Extend = (Array.prototype, {
-			Meow_Shift: function() {
-				if(build.CallFunc) {
-					var m = 0;
-					while(m < build.length) {
-						build[m++] = build[m];
-					}
-					build.length--;
-				} else {
-					build.Meow_base();
-				}
-			},
-			Meow_UnShift: function() {
-				if(build.CallFunc) {
-					var length = Meow_Args.length;
-					var m = build.length += length;
-					while(m--) {
-						build[m] = m < length ? Meow_Args[m] : build[m - length];
-					}
-				} else {
-					build.Meow_base.apply(build, Meow_Args);
-				}
-				return build.length;
-		}
-	});
-});
+    var build = this;
+    var Meow_Extend = (Array.prototype, {
+      Meow_Shift: function() {
+        if (build.CallFunc) {
+          var m = 0;
+          while (m < build.length) {
+            build[m++] = build[m];
+          }
+          build.length--;
+        } else {
+          build.Meow_base();
+        }
+      },
+      Meow_UnShift: function() {
+        if (build.CallFunc) {
+          var length = Meow_Args.length;
+          var m = build.length += length;
+          while (m--) {
+            build[m] = m < length ? Meow_Args[m] : build[m - length];
+          }
+        } else {
+          build.Meow_base.apply(build, Meow_Args);
+        }
+        return build.length;
+      },
+      Meow_Combine: function(Meow_Key, Meow_Val) {
+        if (!Meow_Val) {
+          Meow_Val = Meow_Key;
+        }
+        return Meow_Array.Meow_Reduce(Meow_Key, function(Meow_Hash, Meow_Key, Meow_Index) {
+          Meow_Hash[Meow_Key] = Meow_Val[Meow_Index];
+          return Meow_Hash;
+        }, {});
+      }
+    });
+ });
