bf26ec41472d85cd34d38e0e2f385d924f88dce2
Aswini S <ashumeow@live.com>
2014-12-28 12:17:58 +0530

    Update Meow_Base.js

--- Meow_Base.js
+++ Meow_Base.js
@@ -1,4 +1,26 @@
 var Meow_Base = (function() {
-	
+	Meow_Base.Meow_Extend = function(Meow_Instance, Meow_Static) {
+		var Meow_Extend = Meow_Base.prototype.Meow_Extend;
+		Meow_Base.Meow_protoBuild = true;
+		var build = this;
+		var Meow_proto = new build();
+		Meow_Extend.call(Meow_proto, Meow_Instance);
+		Meow_proto.Meow_Base = function() {};
+		delete Meow_Base.Meow_protoBuild;
+	};
+	//var Meow_Construct = Meow_proto.Meow_Construct.valueOf();
+	var Meow_Construct = Meow_proto.Meow_Construct;
+	var Meow_Class = Meow_proto.Meow_Construct = function() {
+		if(!Meow_Base.Meow_protoBuild) {
+			if(build.Meow_protoConstruct || build.Meow_Construct === Meow_Class) {
+				build.Meow_protoConstruct = true;
+				Meow_Construct.apply(build, Meow_Args);
+				delete build.Meow_protoConstruct;
+			} else if(Meow_Args[0] !== null) {
+				return (Meow_Args[0].Meow_Extend || Meow_Extend).call(Meow_Args[0], Meow_proto);
+			}
+		}
+	};
+
 	// Still coding... Will be updated soon!
 });
