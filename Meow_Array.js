cacfb8bed127ced91edf5ed0d840428d621addd6
Aswini S <ashumeow@live.com>
2014-12-28 21:43:54 +0530

    Update Meow_Array.js

--- Meow_Array.js
+++ Meow_Array.js
@@ -1,7 +1,6 @@
 var Meow_Array = (function() {
 	var build = this;
 	var Meow_Extend = (Array.prototype, {
-		"Chrome": {
 			Meow_Shift: function() {
 				if(build.CallFunc) {
 					var m = 0;
@@ -24,107 +23,6 @@ var Meow_Array = (function() {
 					build.Meow_base.apply(build, Meow_Args);
 				}
 				return build.length;
-			}
-		},
-		"Opera": {
-			Meow_Shift: function() {
-				if(build.CallFunc) {
-					var m = 0;
-					while(m < build.length) {
-						build[m++] = build[m];
-					}
-					build.length--;
-				} else {
-					build.Meow_base();
-				}
-			},
-			Meow_UnShift: function() {
-				if(build.CallFunc) {
-					var length = Meow_Args.length;
-					var m = build.length += length;
-					while(m--) {
-						build[m] = m < length ? Meow_Args[m] : build[m - length];
-					}
-				} else {
-					build.Meow_base.apply(build, Meow_Args);
-				}
-				return build.length;
-			}
-		},
-		"IE": {
-			Meow_Shift: function() {
-				if(build.CallFunc) {
-					var m = 0;
-					while(m < build.length) {
-						build[m++] = build[m];
-					}
-					build.length--;
-				} else {
-					build.Meow_base();
-				}
-			},
-			Meow_UnShift: function() {
-				if(build.CallFunc) {
-					var length = Meow_Args.length;
-					var m = build.length += length;
-					while(m--) {
-						build[m] = m < length ? Meow_Args[m] : build[m - length];
-					}
-				} else {
-					build.Meow_base.apply(build, Meow_Args);
-				}
-				return build.length;
-			}
-		},
-		"Firefox": {
-			Meow_Shift: function() {
-				if(build.CallFunc) {
-					var m = 0;
-					while(m < build.length) {
-						build[m++] = build[m];
-					}
-					build.length--;
-				} else {
-					build.Meow_base();
-				}
-			},
-			Meow_UnShift: function() {
-				if(build.CallFunc) {
-					var length = Meow_Args.length;
-					var m = build.length += length;
-					while(m--) {
-						build[m] = m < length ? Meow_Args[m] : build[m - length];
-					}
-				} else {
-					build.Meow_base.apply(build, Meow_Args);
-				}
-				return build.length;
-			}
-		},
-		"Safari": {
-			Meow_Shift: function() {
-				if(build.CallFunc) {
-					var m = 0;
-					while(m < build.length) {
-						build[m++] = build[m];
-					}
-					build.length--;
-				} else {
-					build.Meow_base();
-				}
-			},
-			Meow_UnShift: function() {
-				if(build.CallFunc) {
-					var length = Meow_Args.length;
-					var m = build.length += length;
-					while(m--) {
-						build[m] = m < length ? Meow_Args[m] : build[m - length];
-					}
-				} else {
-					build.Meow_base.apply(build, Meow_Args);
-				}
-				return build.length;
-			}
 		}
 	});
 });
