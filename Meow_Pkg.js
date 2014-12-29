e75f209fec687b5ee6bc38595af9deb9d9b59883
Aswini S <ashumeow@live.com>
2014-12-29 06:11:13 +0530

    Create Meow_Pkg.js

---
+++ Meow_Pkg.js
@@ -0,0 +1,15 @@
+var MeowPkg = (function() {
+	/*
+	var Meow_Base = MeowNinja('Meow_Base');
+	var Meow_Extend = MeowNinja('Meow_Base');
+	var Meow_Construct = MeowNinja('Meow_Base');
+	*/
+	var Meow_Pkg = Meow_Base.Meow_Extend({
+		Meow_Construct: function(_private, _public) {
+			var build = this;
+			build.Meow_Extend(_public);
+
+			// Still coding... Will be updated soon!
+		}
+	});
+});
