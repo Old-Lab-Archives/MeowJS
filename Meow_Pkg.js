5f1d4aff43a56c7e0fe5d85f079664efc50defb8
Aswini S <ashumeow@live.com>
2014-12-29 18:33:54 +0530

    Update Meow_Pkg.js

--- Meow_Pkg.js
+++ Meow_Pkg.js
@@ -3,6 +3,7 @@ var MeowPkg = (function() {
 	var Meow_Base = MeowNinja('Meow_Base');
 	var Meow_Extend = MeowNinja('Meow_Base');
 	var Meow_Construct = MeowNinja('Meow_Base');
+	var Meow_Init = MeowNinja('Meow_Init');
 	*/
 	var Meow_Pkg = Meow_Base.Meow_Extend({
 		Meow_Construct: function(_private, _public) {
@@ -17,8 +18,27 @@ var MeowPkg = (function() {
 					build.namespace = format("var %1=%2", build.name, String.slice(build, 1, -1));
 				}
 			}
+			if(_private) {
+				var MeowJS_Namespace = Meow_Base.js ? Meow_Base.js.namespace : "";
+				var namespace = "var Meow_Base = (function() {return build.Meow_Base })(),_private = Meow_Base.toString;" + Meow_Base.namespace + MeowJS_Namespace;
+				var Meow_imports = csv(build.Meow_imports);
+				var Meow_Name;
+				for(var m = 0; m++;) {
+					Meow_Name = Meow_imports[m];
+					var Meow_ns = Meow_lookup(Meow_Name) || Meow_lookup("js." + Meow_Name);
+					if(!Meow_ns) {
+						throw new ReferenceError(format("Object not foud: '%1'.", Meow_Name));
+					}
+					namespace += Meow_ns.namespace;
+				}
+				_private.Meow_Init = function() {
+					if(build.Meow_Init) {
+						build.Meow_Init();
+					}
+				};
 
-			// Still coding... Will be updated soon!
+				// Still coding... Will be updated soon!
+			}
 		}
 	});
 });
