d6b977ed59f38d96103c19c4b6c491cb89296f88
Aswini S <ashumeow@live.com>
2014-12-29 10:30:04 +0530

    Update Meow_Pkg.js

--- Meow_Pkg.js
+++ Meow_Pkg.js
@@ -8,6 +8,15 @@ var MeowPkg = (function() {
 		Meow_Construct: function(_private, _public) {
 			var build = this;
 			build.Meow_Extend(_public);
+			if(build.name && build.name !== "Meow_Base") {
+				if(_public.parent === undefined) {
+					build.parent = Meow_Base;
+				}
+				if(build.parent) {
+					build.parent.addName(build.name, build);
+					build.namespace = format("var %1=%2", build.name, String.slice(build, 1, -1));
+				}
+			}
 
 			// Still coding... Will be updated soon!
 		}
