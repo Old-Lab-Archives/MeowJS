bff2f5f9e9963180f46855c8a6fb1f889aac1da2
Aswini S <ashumeow@live.com>
2014-12-31 11:34:56 +0530

    Update meow.js

--- meow.js
+++ meow.js
@@ -1,39 +1,35 @@
 // Main File
 var MeowJS = (function() {
     "use strict";
+    var xMeow = document.createElement('script');
+    xMeow.src = 'MeowNinjaX.js';
+    document.getElementsByTagName("head")[0].appendChild(xMeow);
 
-    // The modules aren't published in NPM registry yet.
-
-    /*
-    var Meow_Hello = MeowNinja('Meow_Hello');
-    var Meow_cli =  MeowNinja('Meow_cli');
-    var Meow_TextSemantics = MeowNinja('Meow_TextSemantics');
-    var Meow_VideoSemantics = MeowNinja('Meow_VideoSemantics');
-    var MeowRangerPred = MeowNinja('Meow_RangerPred');
-    var MeowDOM = MeowNinja('MeowDOM');
-    var MeowString = MeowNinja('MeowString');
-    var MeowBinary = MeowNinja('MeowBinary');
-    var MeowFunText = MeowNinja('MeowFunText');
-    var MeowUTF8 = MeowNinja('MeowUTF8');
-    var MeowUTF8_16 = MeowNinja('MeowUTF8_16');
-    var Meow_ReverseUTF16 = MeowNinja('Meow_ReverseUTF16');
-    var MeowImagePlay = MeowNinja('MeowImagePlay');
-    var MeowImageCache = MeowNinja('MeowImageCache');
-    var HiddenMeow = MeowNinja('HiddenMeow');
-    var MeowStream = MeowNinja('MeowStream'); // H264 Encoder
-    var Meow_DCT = MeowNinja('Meow_DCT');
-    var Meow-DCT-md5 = MeowNinja('Meow-DCT-md5');
-    var Meow_HTTP = MeowNinja('Meow_HTTP');
-    var MeowProxy = MeowNinja('MeowProxy');
-    var Meow_Texture = MeowNinja('Meow_Texture');
-    */
-
-    /*
-    var Meow_Base = MeowNinja('Meow_Base');
-    var Meow_forEach = MeowNinja('forEach');
-    var Meow_Pkg = MeowNinja('Meow_Pkg');
-    var Meow_Array = MeowNinja('Meow_Array');
-    var Meow_Init = MeowNinja('Meow_Init');
-    */
+    MeowNinja.load(
+      "Meow_Hello.js",
+      "Meow_TextSemantics.js",
+      "Meow_VideoSemantics.js",
+      "Meow_DCT.js",
+      "MeowDOM.js",
+      "Meow_RangerPred.js",
+      "MeowFunText.js",
+      "MeowString.js",
+      "MeowUTF8.js",
+      "MeowUTF8_16.js",
+      "Meow_ReverseUTF16.js",
+      "MeowBinary.js",
+      "MeowImagePlay.js",
+      "MeowImageCache.js",
+      "HiddenMeow.js",
+      "MeowStream.js",
+      "Meow-DCT-md5.js",
+      "MeowProxy.js",
+      "Meow_HTTP.js",
+      "Meow_IP.js",
+      "Meow_Base.js",
+      "Meow_forEach.js",
+      "Meow_Pkg.js",
+      "Meow_Array.js",
+      "Meow_Init.js");
   }
 );
