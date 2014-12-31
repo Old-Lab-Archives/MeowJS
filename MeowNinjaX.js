8832b5097d9d9a46ddfe2495cfb9c32ab1860477
Aswini S <ashumeow@live.com>
2014-12-31 07:40:45 +0530

    Update MeowNinjaX.js

--- MeowNinjaX.js
+++ MeowNinjaX.js
@@ -19,7 +19,7 @@ var MeowNinjaX = (function(Meow_WinWin, undefined) {
 	var Meow_IsDOMReady;
 	var Ninja = Meow_WinWin.MeowNinja_Conf && Meow_WinWin.MeowNinja_Conf.MeowNinja || "MeowNinja";
 	var MeowNinja_API = Meow_WinWin[Ninja] = (Meow_WinWin[Ninja] || function() {
-		MeowNinja_API.Meow_Ready.apply(null, Meow_Args);
+		MeowNinja_API.ready.apply(null, Meow_Args);
 	});
 	var Meow_Preloading = 1;
 	var Meow_Preloaded = 2;
@@ -41,6 +41,47 @@ var MeowNinjaX = (function(Meow_WinWin, undefined) {
 		var Meow_Class = Object.prototype.toString.call(Meow_Obj).slice(8, -1);
 		return Meow_Obj !== undefined && Meow_Obj !== null && Meow_Class === Meow_Type;
 	}
+	function Meow_isFunc(Meow_Item) {
+		return Meow_Meow("Function", Meow_Item);
+	}
+	function Meow_Array(Meow_Item) {
+		return Meow_Meow("Array", Meow_Item);
+	}
+	function Meow_ToLabel(Meow_url) {
+		var Meow_Items = Meow_url.split("/");
+		var Meow_Name = Meow_Items[Meow_Items.length - 1];
+		var m = Meow_Name.indexOf("?");
+		return m !== -1 ? Meow_Name.substring(0, m) : Meow_Name;
+	}
+	function Meow_Unity(Meow_Callback) {
+		Meow_Callback = Meow_Callback || Meow_Idle;
+		if(Meow_Callback.Meow_Finished) {
+			return;
+		}
+		Meow_Callback();
+		Meow_Callback.Meow_Finished = 1;
+	}
+	function Meow_ConditionType(Meow_Test, Meow_Success, Meow_Failure, Meow_Callback) {
+		var Meow_Obj = (typeof Meow_Test === "object") ? Meow_Test : {
+			Meow_Test : Meow_Test,
+			Meow_Success : !!Meow_Success ? Meow_Array(Meow_Success) ? Meow_Success : [Meow_Success] : false,
+			Meow_Failure : !!Meow_Failure ? Meow_Array(Meow_Failure) ? Meow_Failure : [Meow_Failure] : false,
+			Meow_Callback : Meow_Callback || Meow_Idle
+		};
+		var Meow_Passed = !!Meow_Obj.Meow_Test;
+		if(Meow_Passed && !!Meow_Obj.Meow_Success) {
+			Meow_Obj.Meow_Success.push(Meow_Obj.Meow_Callback);
+			MeowNinja_API.load.apply(null, Meow_Obj.Meow_Success);
+		} else if(!Meow_Passed && !!Meow_Obj.Meow_Failure) {
+			Meow_Obj.Meow_Failure.push(Meow_Obj.Meow_Callback);
+			MeowNinja_API.load.apply(null, Meow_Obj.Meow_Failure);
+		} else {
+			Meow_Callback();
+		}
+		return MeowNinja_API;
+	}
+	function Meow_FetchAsset(Meow_Item) {
 
-	// Still coding... Will be updated soon!
+		// Still coding... Will be updated soon!
+	}
 });
