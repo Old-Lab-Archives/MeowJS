07c0e9984eed4e4aab7f00d44e3023af96367c21
Aswini S <ashumeow@live.com>
2015-01-02 06:20:34 +0530

    Create Meow_Path.js

---
+++ Meow_Path.js
@@ -0,0 +1,9 @@
+var Meow_Process = Meow_Process || {};
+var Meow_Path = (function() {
+	"use strict";
+	var Meow_isWin = Meow_Process.platform === 'win32';
+	function Meow_ArrayNormalize(Meow_Parts, Meow_AllowAbvRoot) {
+
+		// Still coding now... Will be updated soon!
+	}
+});
