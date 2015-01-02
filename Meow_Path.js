8a46c9b2cf3a810057dbfa3655ca7ee57af204ba
Aswini S <ashumeow@live.com>
2015-01-02 08:03:32 +0530

    Update Meow_Path.js

--- Meow_Path.js
+++ Meow_Path.js
@@ -1,7 +1,8 @@
 var Meow_Process = Meow_Process || {};
-var Meow_Path = (function() {
+var Meow_Path0 = (function() {
 	"use strict";
 	var Meow_isWin = Meow_Process.platform === 'win32';
+	var Meow_Args = arguments;
 	function Meow_ArrayNormalize(Meow_Parts, Meow_AllowAbvRoot) {
 		var Meow_Up = 0;
 		for(var m = Meow_Parts.length; m >= 0; m--) {
@@ -21,6 +22,21 @@ var Meow_Path = (function() {
 		}
 		return Meow_Parts;
 	}
+	if(Meow_isWin) {
+		var Meow_PathSplit = /^(.+(?:[\\\/](?!$)|:)|[\\\/])?((?:.+?)?(\.[^.]*)?)$/;
+		var Meow_DeviceSplit = /^([a-zA-Z]:|[\\\/]{2}[^\\\/]+[\\\/][^\\\/]+)?([\\\/])?(.*?)$/;
+		exports.Meow_Resolve = function() {
+			var DeviceResolve = '';
+			var TailResolved = '';
+			var AbsoluteResolved = false;
+			for(var m = Meow_Args.length; m >= -1; m--) {
+				var Meow_Path = (m >= 0) ? Meow_Args[m] : Meow_Process.cwd();
+				if(typeof Meow_Path !== "string" || !Meow_Path) {
+					continue;
+				}
 
-	// Still coding now... Will be updated soon!
+				// Still coding now... Will be updated soon!
+			}
+		} 
+	}
 });
