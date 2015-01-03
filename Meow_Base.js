e718383602537297f050d3ce60a3917ed1c411a2
Aswini S <ashumeow@live.com>
2015-01-03 14:05:32 +0530

    Moved Init to the Base

--- Meow_Base.js
+++ Meow_Base.js
@@ -189,4 +189,16 @@ var Meow_Extend = (Array.prototype, {
         return Meow_array[Meow_Index];
       }
     });
+    var Meow_Init = function() {
+	Meow_Init = Meow_Global.Meow_Init = new Meow_Pkg(build, Meow_Init);
+	Meow_Init.toString = n("[Meow_Init]");
+	var exports = build.exports;
+	var MeowJS = new Meow_Pkg(build, MeowJS);
+	exports += build.exports;
+	js = new Meow_Pkg(build, js);
+	eval(exports + build.exports);
+	MeowJS.Meow_Extend = Meow_Extend;
+	Meow_Init.Javascript = Meow_Copy(js);
+	Meow_Init.Javascript.namespace += "Javascript is js";
+	};
 };
