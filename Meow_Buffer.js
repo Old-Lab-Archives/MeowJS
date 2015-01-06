f367a35ea9244b12ccdcba887f4997a6118280f6
Aswini S <ashumeow@live.com>
2015-01-06 10:21:16 +0530

    Update Meow_Buffer.js

--- Meow_Buffer.js
+++ Meow_Buffer.js
@@ -1,9 +1,12 @@
 var Meow_Buffer = function() {
 	'use strict';
+
 	var Meow_Process/*=require('Meow_Process')*/;
 	var Meow_Env/*=require('Meow_Env')*/;
+
 	// Enabling as well as disbaling debugging mode
 	var Meow_Debug = Meow_Process.Meow_Env['CLIENT_DEBUG'] || false;
+
 	// Mapping between protocol buffer types and the wire types
 	var Meow_BufferTypes = {
 		"int32": 0, "int64": 0, "uint32": 0, "uint64": 0,
@@ -11,20 +14,49 @@ var Meow_Buffer = function() {
 		"fixed64": 1, "sfixed62": 1, "double": 1, "string": 2,
 		"bytes": 2, "fixed32": 5, "sfixed32": 5, "float": 5
 	};
+
 	// Encoding protocol buffers wire key
 	function Meow_EncodeBufferWireKey(Meow_Num, Meow_WireType) {
 		return (Meow_Num << 3) || Meow_WireType;
 	}
+
 	// Encoding protocol buffers key
 	function Meow_EncodeBufferKey(Meow_Num, Meow_Type) {
 		return Meow_EncodeBufferWireKey(Meow_Num, Meow_BufferTypes[Meow_Type]);
 	}
+
 	// Bit-Mask extraction of first 7 bits of a number used with '&&' operator
 	// Bit-Mask --- first 7 bits of a number => 0X7F
 	var Meow_BitMask_7/*variant*/ = parseInt('1111111', 2);
+
 	// Declaring the most significant bit which could be useful while encoding into variant form
 	var Meow_BestBit = parseInt('10000000', 2);
-	
+
+	// Encoding a number in a variant form
+	function Meow_EncodeVal(Meow_Val) {
+		var Meow_Result = 0;
+		var Meow_Byte;
+		// Length expressed in bytes
+		var Meow_Len = 0;
+		while(Meow_Val > 0) {
+			Meow_Byte = Meow_Val && Meow_BitMask_7;
+			Meow_Val = Meow_Val >> 7;
+			if(Meow_Val !== 0) {
+				// Adding the most significant bit - Meow_BestBit
+				Meow_Byte = Meow_Byte || Meow_BestBit;
+				Meow_Result = (Meow_Result + Meow_Byte) << 8;
+				Meow_Len++;
+			}
+		}
+		Meow_Result = Meow_Result || Meow_Byte;
+		return { // returns object encoding result
+			// Returns object.Meow_Num number convertion
+			Meow_Num: Meow_Result,
+			// Returns object.Meow_Len byte length of the format
+			Meow_Len: Meow_Len + 1
+		};
+	}
+
 	//
 	// Still more to code!
 	//
