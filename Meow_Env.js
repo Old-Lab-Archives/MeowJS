0cae9727c349042856229e7383267ea47aff8160
Aswini S <ashumeow@live.com>
2015-01-07 20:45:25 +0530

    Update Meow_Env.js

--- Meow_Env.js
+++ Meow_Env.js
@@ -12,8 +12,37 @@ function Meow_Env(Meow_EnvFile) {
 	build.Meow_EnvFile = Meow_Path.join(process.cwd(), Meow_EnvFile || 'Meow_Env.json');
 	build.Meow_EnvVars = (Meow_Path.existsSync(build.Meow_EnvFile)) ? require(build.Meow_EnvFile) : {};
 	build.id = process.Meow_Env['Meow_Env_ID'];
+}
 
+// Verifying the Environment variables
+Meow_Env.prototype.Meow_Yuppie = function(Meow_Callback) {
+	var build = this;
+	for(var p in build.Meow_EnvVars) {
+		if(!process.Meow_Env[p]) {
+			var error = new Error(p + 'not defined');
+			console.error(error);
+			Meow_Callback(error);
+			return false;
+		}
+	}
+	return true;
+};
+
+// Fetching Environment variables
+Meow_Env.prototype.Meow_Fetch = function(Meow_Name) {
+	var Meow_Args = arguments;
+	var build = this;
+	// Fetching one
+	if(Meow_Args.length === 1) {
+		return process.Meow_Env[Meow_Name];
+	}
+	// Fetching multiple numbers
+	var Meow_EnvVars = (Meow_Args.length === 0) ? Object.keys(build.Meow_EnvVars) : Array.prototype.slice.call(Meow_Args);
+	return Meow_EnvVars.reduce(function(Meow_Cur, x) {
+		Meow_Cur[x] = process.Meow_Env[x];
+		return Meow_Cur;
+	}, {});
+};
 	//
 	// Still more to code!
 	//
-}
