be66ca8230e793d54e7ed1eaa95d27081e6a5b65
Aswini S <ashumeow@live.com>
2015-01-08 20:56:44 +0530

    Update HiddenMeow.js

--- HiddenMeow.js
+++ HiddenMeow.js
@@ -1,11 +1,11 @@
 var HiddenMeow = function() {
   "use strict";
+  var m, m2, m3, xxx;
+  var Meow_String;
+  var Meow_Pick;
+  var Meow_Object;
   var Meow_Power, Meow_JSON, HiddenMeow1, HiddenMeow2, Meow_HelloAlpha, Meow_HelloBeta, Meow_HelloKappa, Meow_Char, Meow_HelloInput, Meow_Nodes;
     HiddenMeow.Meow_Predict = function(x, a) {
-      var m, m2, m3, xxx;
-      var Meow_String;
-      var Meow_Pick;
-      var Meow_Object;
       Meow_Power.Meow_Nodes = [];
       Meow_Power.Meow_Init = [];
       Meow_Power.Meow_Char = x;
