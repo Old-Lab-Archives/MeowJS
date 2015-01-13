726075754ab7c84286211e98e7a92a8779233fff
Aswini S <ashumeow@live.com>
2015-01-13 19:52:18 +0530

    Update Meow_SVG_FastScroll.js

--- Meow_SVG_FastScroll.js
+++ Meow_SVG_FastScroll.js
@@ -1,6 +1,7 @@
 var meowSVG_FastScroll = function() {
 	"use strict";
 	var Meow_Power = this;
+	var Meow_Loc;
 	meowSVG_FastScroll('a[href*=#]:not([href=#]').click(function() {
 		if(Meow_Loc.pathname.replace(/^\//,'') === Meow_Power.pathname.replace(/^\//,'')) {
 			var Meow_Target = meowSVG_FastScroll(Meow_Power.hash);
