892c79278a3057d019bf0fd1790c2b23334337f7
Aswini S <ashumeow@live.com>
2015-01-13 20:31:54 +0530

    Shifted image cache to the loader

--- MeowDCT_ImageLoader.js
+++ MeowDCT_ImageLoader.js
@@ -1,3 +1,6 @@
+var Meow_ImageLoader = function() {
+
+// MeowJS DCT Image Loader
 var MeowDCT_ImageLoader = function() {
   "use strict";
     var Meow_BlockSize = 8;
@@ -93,4 +96,36 @@ var MeowDCT_ImageLoader = function() {
       Meow_OnChangeImg: Meow_OnChangeImg,
       Meow_OnChangeCoeff: Meow_OnChangeCoeff
     };
+};
+// End of MeowJS DCT image loader
+
+// MeowJS Image Cache
+var MeowImageCache = function() {
+  "use strict";
+    MeowImageCache.Meow_ImageCache = function() {
+      var Meow_ImageCache = [];
+      var Meow_Image;
+      var Meow_CacheRoot = document.location.href.split('/');
+      Meow_CacheRoot.pop();
+      Meow_CacheRoot = Meow_CacheRoot.join('/') + '/';
+      var Meow_Power = function() {
+      Meow_Power.Meow_Push = function(src, meowLoadEvent) {
+        if (!src.match(/^http/)) {
+          src = Meow_CacheRoot + src;
+        }
+        var Meow_ImageItem = new Meow_Image();
+        if (Meow_ImageCache[src] && meowLoadEvent) {
+          meowLoadEvent(src);
+        } else {
+          if (meowLoadEvent) {
+            Meow_ImageItem.Meow_OnLoad = meowLoadEvent;
+            Meow_ImageItem.Meow_OnError = meowLoadEvent;
+          }
+          Meow_ImageCache[src] = Meow_ImageItem;
+        }
+        Meow_ImageItem.src = src;
+      }; };
+    };
+};
+// End of MeowJS Image Cache
 };
