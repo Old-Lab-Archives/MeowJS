6a13da5821d2c497c1466f10f4f172425e147c1f
Aswini S <ashumeow@live.com>
2015-01-18 20:52:52 +0530

    Update Meow_AudioCodec.js

--- Meow_AudioCodec.js
+++ Meow_AudioCodec.js
@@ -1,7 +1,30 @@
 var Meow_AudioCodec = function(x, meowCodec) {
 	'use strict';
-
-	//
-	// Still more to code
-	//
+	var meowAudioLib, meowAudioDevice;
+	var meowEncode, meowDecode;
+	var record, xSample;
+	var Meow_Data;
+	var Meow_SampleRate;
+	var Meow_ChannelCount;
+	var Meow_Power = this;
+	var xx = x[0].toUpperCase() + x.substr(1).toLowerCase();
+	Meow_AudioCodec[x] = meowCodec;
+	// encode
+	if(meowCodec.meowEncode) {
+		meowAudioLib.meowAudioDevice.record.prototype['to' + xx] = function(meowBytesPerSample) {
+			return Meow_AudioCodec[x].meowEncode({
+				Meow_Data: Meow_Power.join(),
+				Meow_SampleRate: Meow_Power.boundTo.Meow_SampleRate,
+				Meow_ChannelCount: Meow_Power.boundTo.Meow_ChannelCount,
+				meowBytesPerSample: meowBytesPerSample
+			});
+		};
+	}
+	// decode
+	if(meowCodec.meowDecode) {
+		meowAudioLib.xSample.prototype['load' + xx] = function(Meow_FileData) {
+			Meow_Power.load.apply(Meow_Power, [Meow_AudioCodec[x].meowDecode(Meow_FileData)].concat([].slice.call(arguments, 1)));
+		};
+	}
+	return meowCodec;
 };
