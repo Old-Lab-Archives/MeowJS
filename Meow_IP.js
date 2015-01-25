8bc2ca2f562f053a23a4bae92e1d68f896240cff
Aswini S <ashumeow@live.com>
2015-01-25 11:20:31 +0530

    Update Meow_IP.js

--- Meow_IP.js
+++ Meow_IP.js
@@ -1,6 +1,7 @@
 var Meow_IP = function() {
-	"use strict";
+	'use strict';
 	// Matching IP addresses
+	var Meow_RegEx;
 	var meow_IPv4 = '(?:25[0-5]|2[0-4][0-9]|1?[0-9][0-9]{1,2}|[0-9]){1,}(?:\\.(?:25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}|0)){3}';
 	var meow_IPv6 = '(?:(?:[0-9a-fA-F:]){1,4}(?:(?::(?:[0-9a-fA-F]){1,4}|:)){2,7})+';
 	Meow_IP = module.exports = function(Meow_Opts) {
