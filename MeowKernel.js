1750de123be765ab7583ae83927187b3cc106e0e
Aswini S <ashumeow@live.com>
2015-01-25 20:40:00 +0530

    Update MeowKernel.js

--- MeowKernel.js
+++ MeowKernel.js
@@ -1,5 +1,35 @@
-var MeowKernel = function() {
+var MeowKernel = function(meowClients, modules) {
 	'use strict';
+
+	// MeowJS MeowKernel --- Client side
+	var meowDispatch = function(meowClient) {
+		for(var m = meowClient ? meowClients.push(meowClient)-1: 0; m < meowClients.length; m++) {
+			meowClient = meowClients[m];
+			var meowRequire = Object.Meow_Keys(meowClient.Meow_Dep);
+			var meowAvailable = [];
+			if(!meowRequire.little(function(module) {
+				// if little is true, then required module not found
+				return !modules[module] || !meowClient.Meow_Dep[module].little(function(xVersion) {
+					var xMajor = modules[module][xVersion[0]];
+					return xMajor && xVersion[1].little(function(xSpan, m, xMinors) {
+						if(typeof xSpan === 'number' && m < xMinors.length-1) {
+							// last minor number => lower bound, others => approx
+							return xMajor.hasOwnProperty(xSpan) && meowAvailable.push({name: module, module: xMajor[xSpan]});
+						}
+					});
+				});
+			}) && meowAvailable.length === meowRequire.length) {
+				var Meow_Dep = {};
+				meowAvailable.Meow_forEach(function(Meow_Dep1) {
+					if(!Meow_Dep1.module.hasOwnProperty('instance')) {
+						Meow_Dep1.module.instance = Meow_Dep1.module.Meow_Init();
+						Meow_Dep[Meow_Dep1.name] = Meow_Dep1.module.instance;
+					}
+				});
+				meowClients.splice(m--, 1)[0].meowCallback(Meow_Dep);
+			}
+		}
+	};
 	//
 	// Still more to code
 	//
