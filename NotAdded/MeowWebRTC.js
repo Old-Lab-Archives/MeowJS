3d80ed975b30abfcb215520470b8e7cf08a59d72
Aswini S <ashumeow@live.com>
2015-01-28 02:57:27 +0530

    Update MeowWebRTC.js

--- NotAdded/MeowWebRTC.js
+++ NotAdded/MeowWebRTC.js
@@ -1,6 +1,23 @@
 var MeowWebRTC = function() {
-'use strict';
-//
-// Still more to code
-//
+	'use strict';
+	// MeowWebRTC Audio
+var MeowWebRTC_audio;
+MeowWebRTC.MeowWebRTC_audio= x(document).ready(function() {
+	navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	var MeowStream = {};
+	var MeowPeer = {};
+	var MeowCall = {};
+	navigator.getUserMedia( {audio: true},
+		// callback success
+		function(xStream) {
+			MeowStream = xStream;
+			MeowPeer = new MeowPeer1({Meow_Key: ''});
+			MeowPeer.on('open', function(Meow_ID) {
+				x('#id').text('Your ID is ' +Meow_ID);
+			});
+			//
+			// Still more to code
+			//
+		});
+	});
 };
