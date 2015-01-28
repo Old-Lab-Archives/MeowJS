4227522d1ea7a793ab90ad92c8c952ed2550c1b4
Aswini S <ashumeow@live.com>
2015-01-28 16:54:04 +0530

    Update MeowWebRTC_stream.js

--- NotAdded/MeowWebRTC_stream.js
+++ NotAdded/MeowWebRTC_stream.js
@@ -1,6 +1,58 @@
 var MeowWebRTC_stream = function() {
 'use strict';
-//
-// Still more to code
-//
+var build = this;
+var MeowStreamX;
+var util;
+function MeowDataStream(MeowChannel, Meow_Opts) {
+	if(!(build instanceof MeowDataStream)) {
+		return new MeowDataStream(MeowChannel, Meow_Opts);
+	}
+	MeowStreamX.MeowStream.call(build);
+	build.Meow_Opts = Meow_Opts || {};
+	build.MeowRead = true;
+	build.MeowWrite = true;
+	build.Meow_Buffer = [];
+
+	// binding events
+	build.MeowRTC = MeowChannel;
+	build.MeowRTC.addEventListener('message', build.onMessage.bind(build));
+	build.MeowRTC.onerror = build.onError.bind(build);
+	build.MeowRTC.onclose = build.onClose.bind(build);
+	build.MeowRTC.onopen = build.onOpen.bind(build);
+	if(build.MeowRTC.readyState === 'open') {
+		build.open = true;
+	}
+	util.inherits(MeowDataStream, MeowStreamX.MeowStream);
+
+	// exporting
+	module.exports = MeowDataStream;
+	module.exports.MeowDataStream = MeowDataStream;
+
+	MeowDataStream.prototype.onMessage = function(x, Meow_Flags) {
+		var Meow_Data = e;
+		if(Meow_Data.data) {
+			Meow_Data = Meow_Data.data;
+		}
+
+		// Type in the form of types array => Array Buffer View
+		var Meow_Type = build.options.type;
+		if(Meow_Type && Meow_Data instanceof ArrayBuffer) {
+			Meow_Data = new Meow_Type(Meow_Data);
+		}
+		build.emit('data', Meow_Data, Meow_Flags);
+	};
+	MeowDataStream.prototype.onError = function(err) {
+		build.emit('error', err);
+	};
+	MeowDataStream.prototype.onClose = function(err) {
+		if(build.destroy) {
+			return;
+		}
+		build.emit('end');
+		build.emit('close');
+	};
+	//
+	// Still more to code
+	//
+	}
 };
