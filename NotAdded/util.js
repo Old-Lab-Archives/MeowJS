c3a240e9efb98f3e6849d6be5f862e40015ae81d
Aswini S <ashumeow@live.com>
2015-01-30 04:34:25 +0530

    Update util.js

--- NotAdded/util.js
+++ NotAdded/util.js
@@ -1,6 +1,16 @@
 var util = function() {
-'use strict';
-//
-// Still more to code!
-//
+	'use strict';
+	var MeowRegEx = /%[sdj%]/g;
+	exports.format = function(fo) {
+		if(!MeowisStr(fo)) {
+			var objects = [];
+			for(var m = 0; m < arguments.length; m++) {
+				objects.push(inspect(arguments[m]));
+			}
+			return objects.join(' ');
+		}
+		//
+		// Still more to code
+		//
+	}
 };
