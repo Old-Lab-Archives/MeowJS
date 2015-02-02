357406c57f81f370bce128c31afe25b672bf85f1
Aswini S <ashumeow@live.com>
2015-02-02 08:10:35 +0530

    Update MeowEventProxy.js

--- NotAdded/MeowEventProxy.js
+++ NotAdded/MeowEventProxy.js
@@ -1,6 +1,24 @@
 var MeowEventProxy = function() {
 	'use strict';
+	var xxx = this;
+	// global definition
+	MeowEventProxy.$ = function(name, definition) {
+	// checking define
+	var MeowDefn = typeof define === 'function';
+	// checking exports
+	var MeowExports = typeof module !== 'undefined' && module.exports;
+	if(MeowDefn) {
+		// AMD module or CMD module
+		define(MeowDefn);
+	} else {
+		// assigning common namespaces or global object
+		xxx[name] = definition();
+	}
+	}('MeowEventProxy', function (debug) {
+		// debugging
+		debug = debug || function() {};
 	//
 	// Still more to code
 	//
+	});
 };
