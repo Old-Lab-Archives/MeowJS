0935062774b886bd634ab6c7924d4bc67ffb165d
Aswini S <ashumeow@live.com>
2015-02-05 01:13:53 +0530

    Update meowNinja.js

--- meowNinja.js
+++ meowNinja.js
@@ -2,6 +2,7 @@
 	function meowNinja(src, cb) {
 		// src => source
 		// cb => callback
+		var window;
 		var ref = window.document.getElementsByTagName("script")[0];
 		var script = window.document.createElement("script");
 		script.src = src;
