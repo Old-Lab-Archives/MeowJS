3a202f3df94a32599c2c5d7e5a95c0fb552243f4
Aswini S <ashumeow@live.com>
2015-02-05 00:47:09 +0530

    loader

---
+++ meowNinja.js
@@ -0,0 +1,14 @@
+// MeowJS JS file Loader
+	function meowNinja(src, cb) {
+		// src => source
+		// cb => callback
+		var ref = window.document.getElementsByTagName("script")[0];
+		var script = window.document.createElement("script");
+		script.src = src;
+		script.async = true;
+		ref.parentNode.insertBefore(script, ref);
+		if(cb && typeof(cb) === "function") {
+			script.onLoad = cb;
+		}
+		return script;
+	}
