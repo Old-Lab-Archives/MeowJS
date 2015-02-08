5c15d8357b8276c16f31687b025b2ab3a7d5655e
Aswini S <ashumeow@live.com>
2015-02-08 20:29:50 +0530

    Update MeowWebRTC_audio.js

--- tests/js/MeowWebRTC_audio.js
+++ tests/js/MeowWebRTC_audio.js
@@ -58,13 +58,14 @@ var MeowWebRTC = function() {
 	 	MeowPeerAudio.attr('src', URL.createObjectURL(MeowPeerStream));
 	 	MeowPeerAudio.get(0).play();
 	 };
-	 x('#chorus-format').submit(function(format) {
+	 x('#audio-format').submit(function(format) {
 	 	var song = x(format.target)[0][0].value;
 	 	var chorus = x(format.target)[0][1].value;
 	 	var verse = x(format.target)[0][2].value;
 	 	var html = '<h3>'+ song + '</h3>';
-	 	html += '<h4>Chorus: '+chorus+ '</h4>';
-	 	html += '<h4>Verse: '+verse+ '</h4>';
+	 	html += '<h4>Artist: '+artist+ '</h4>';
+	 	html += '<h4>Track: '+track+ '</h4>';
+	 	html += '<h4>Year:' +year+ '</h4>';
 	 	x('#song-info').meowAppend(html);
 	 	return false;
 	 	});
