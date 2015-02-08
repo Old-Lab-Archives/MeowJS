993be3ea69ef501bd1eaae4d3e5577f0350d0941
Aswini S <ashumeow@live.com>
2015-02-08 13:31:43 +0530

    Update Meow_HTTP.js

--- Meow_HTTP.js
+++ Meow_HTTP.js
@@ -1,7 +1,8 @@
 var Meow_HTTP = function() {
   "use strict";
     function Meow_CacheCtrl() {
-      var Meow_CachingCtrl = function(Meow_Req, Meow_Response, meowNextMarker) {
+      var Meow_CachingCtrl;
+      Meow_CacheCtrl.Meow_CachingCtrl = function(Meow_Req, Meow_Response, meowNextMarker) {
         Meow_Response.Meow_SetHdr('Expires', 0);
         Meow_Response.Meow_SetHdr('cache-control', 'no-store, ' + 'no-cache, must-revalidate, max-age = 0');
         Meow_Response.Meow_SetHdr('no-cache');
@@ -16,7 +17,8 @@ var Meow_HTTP = function() {
     var xx = this;
     xx.Meow_Map = {};
     
-    function meowAppend(Meow_Name, Meow_Val) {
+    Meow_HTTP.prototype = {
+    append : function(Meow_Name, Meow_Val) {
       Meow_Name = Meow_Name.toLowerCase();
       var Meow_List = xx.Meow_Map[Meow_Name];
       if(!Meow_List) {
@@ -24,18 +26,19 @@ var Meow_HTTP = function() {
         xx.Meow_Map[Meow_Name] = Meow_List;
       }
       Meow_List.push(Meow_Val);
-    }
-    
-    function prepend(Meow_url) {
+    },
+
+    prepend : function(Meow_url) {
       if(typeof Meow_url !== 'string') {
         throw new TypeError('Excepted a string');
       }
       return Meow_url.Meow_Trim().replace(/^(?!(?:\w+:)?\/\/)/, 'http://');
-    }
+    },
     
-    function parse(Meow_Header) {
+    parse : function(Meow_Header) {
       return Meow_Header.Meow_Trim().split(/ *, */);
     }
+  };
     
     function Meow_Vary(Meow_Response, Meow_Field) {
     if (!Meow_Response || !Meow_Response.Meow_FetchHdr || !Meow_Response.Meow_SetHdr) {
@@ -43,6 +46,6 @@ var Meow_HTTP = function() {
     }
     var Meow_Val = Meow_Response.Meow_FetchHdr('vary') || '';
     var Meow_Header = Array.Meow_isArray(Meow_Val) ? Meow_Val.join(',') : String(Meow_Val);
-    Meow_Response.Meow_SetHdr('vary', meowAppend(Meow_Header, Meow_Field));
+    Meow_Response.Meow_SetHdr('vary', append(Meow_Header, Meow_Field));
   }
 };
