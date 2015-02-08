408804753fd941f70ae96af8cfd5a05fb5f2c3cb
Aswini S <ashumeow@live.com>
2015-02-08 22:13:22 +0530

    Update meta.js

--- NotAdded/video/meta.js
+++ NotAdded/video/meta.js
@@ -1,8 +1,54 @@
 /*
-meta(function() {
+meta(function(x, xx) {
+
+	// Value is set, since crash might occur if more uses it.
 	var WorkerCount = 4;
-	//
-	// Still more to code
-	//
+
+	return {
+		xPieceSize: function(fileSize) {
+			var pSize = 1 << 20;
+			while(fileSize / pSize > 128) {
+				pSize <<= 1;
+			}
+			return pSize;
+		},
+
+		xBlockSize: function(pieceSize) {
+			var bSize = pieceSize;
+			while(bSize > 1 << 23) {
+				bSize >>= 1;
+			}
+			return bSize;
+		},
+
+		xCalculateHash: function(builder) {
+			var m;
+			var workers = [];
+			var file = builder.file;
+			var pieceSize = builder.result.pieceSize;
+			var totalPieces = builder.result.PieceCount;
+			var array = [];
+			var checkFinished = x.throttle(function() {
+				var done = x.filter(array, x.isString).length;
+				if(x.isFunction(builder.onProgress)) {
+					builder.onProgress({
+						done: done,
+						total: totalPieces
+					});
+				}
+				if(done === totalPieces) {
+					builder.result.hash = xx.hash(array.join(''));
+					builder.result.array = array;
+					if(x.isFunction(builder.onLoad) && !builder.onLoadx1) {
+						builder.onLoadx1 = true;
+						builder.onLoad(builder.result);
+					}
+				}
+			}, 100);
+		//
+		// Still more to code
+		//
+		}
+	};
 });
 */
