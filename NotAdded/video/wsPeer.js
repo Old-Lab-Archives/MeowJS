a8b1b510e37ab0d196afa4bd383e396db3920856
Aswini S <ashumeow@live.com>
2015-02-09 21:40:30 +0530

    Create wsPeer.js

---
+++ NotAdded/video/wsPeer.js
@@ -0,0 +1,16 @@
+// wsPeer => Web Socket Peer
+function wsPeer(url, client) {
+	var ig = this;
+	ig.id = url;
+	ig.url = url;
+	ig.client = client;
+	ig.init();
+}
+var ig = this;
+wsPeer.prototype = {
+	init: function() {
+		//
+		// Still more to code
+		//
+	}
+};
