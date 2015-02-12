b0682d5231059c492b54cabefeead523d3403442
Aswini S <ashumeow@live.com>
2015-02-13 02:46:08 +0530

    Update p2p.js

--- NotAdded/video/p2p.js
+++ NotAdded/video/p2p.js
@@ -38,12 +38,86 @@ define(['peer', 'wsPeer', 'httpPeer', 'sys', 'xx'], function(peer, hpeer, wsPeer
 			ig.peerTrans = {};
 			ig.lastSpeedReport = now();
 			var speedReportInterval = setInterval(x.bind(ig.speedReport, ig), 1000);
-			ig.ws = new WebSocket((location.protocol == 'https:' ? 'wss://': 'ws://')+location.host+'/room/ws');
+			ig.ws = new WebSocket((location.protocol === 'https:' ? 'wss://': 'ws://')+location.host+'/room/ws');
 			ig.ws.onOpen = x.bind(ig.onWsOpen, ig);
 			ig.ws.onMessage = x.bind(ig.onWsMessage, ig);
 		},
+		newLink: function(meta, callback) {
+			ig.ws.send(JSON.stringify({
+				cmd: 'newLink',
+				meta: meta
+			}));
+		},
+		enterLink: function(linkID) {
+			ig.ws.send(JSON.stringify({
+				cmd: 'enterLink',
+				linkID: linkID
+			}));
+		},
+		httpPeerAdd: function(url) {
+			ig.ws.send(JSON.stringify({
+				cmd: 'httpPeerAdd',
+				url: url,
+				bitmap: client.finishedPiece.join('')
+			}));
+		},
+		updatePeerList: function() {
+			ig.ws.send(JSON.stringify({
+				cmd: 'getPeerList'
+			}));
+		},
+		updateBitmap: function() {
+			ig.ws.send(JSON.stringify({
+				cmd: 'updateBitmap',
+				bitmap: client.finishedPiece.join('')
+			}));
+		},
+		strength: function() {
+			if(!ig.meta) {
+				return 0;
+			}
+			var i;
+			var temp = [];
+			for(m = 0; m < ig.meta.pieceCount; m++) {
+				temp.push(0);
+			}
+			x.each(this.peerList, function (value, key) {
+				for(m = 0; m < ig.meta.pieceCount; m++) {
+					temp[m] += (value.bitmap[m] === '1' ? 1 : 0);
+				}
+			});
+			var min = x.min(temp);
+			return min+(x.filter(temp, function (num) {
+				return num > min;
+			}).length / temp.length);
+		},
+		// exporting
+		onReady: function() {
+			console.log('onReady');
+		},
+		onMeta: function(meta) {
+			console.log('onMeta', meta);
+		},
+		onPeerList: function(peerList) {
+			console.log('onPeerList', peerList);
+		},
+		onPeerConnect: function(peer) {
+			console.log('onNewPeer', peer);
+		},
+		onPeerDisconnect: function(peer) {
+			console.log('onNewPeer', peer);
+		},
+		onPiece: function(piece) {
+			console.log('onPiece', piece);
+		},
+		onFinished: function() {
+			console.log('onFinished');
+		},
+		onSpeedReport: function(report) {
+			console.log('onSpeedReport', report);
+		},
 		//
-		// Still more to code
+		// Still more to code!
 		//
 	};
 });
